fa4b9acf111281cd456e3ff65dd9ab3fd4fb7b26
Michael <68960336+Mikewon98@users.noreply.github.com>
2024-03-16 16:46:57 +0300

    fixed minor issue

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,19 +9,19 @@ const HomePage = () => {
   var phoneNumber = state?.inputValue;
 
   return (
-    <div className='m-2 flex flex-col lg:h-screen'>
-      <div className='flex-none h-10 bg-green-400 rounded flex items-center justify-center gap-2'>
+    <div className=' flex flex-col fixed top-0 left-0 w-full h-full items-center justify-center'>
+      <div className='flex-none h-10 w-full bg-green-400 rounded flex items-center justify-center gap-2'>
         <p>46L,</p>
         <p>French latte cafe,</p>
         <p>3/16/2024,</p>
       </div>
-      <div className='flex-grow h-96 bg-orange-400 rounded my-2 flex items-center justify-center '>
+      <div className='flex-grow h-96 w-full bg-orange-400 rounded my-2 flex items-center justify-center '>
         <p className='text-4xl text-white'>Menu</p>
       </div>
-      <div className='flex-none h-10 bg-green-400 rounded mb-2 flex items-center justify-center '>
+      <div className='flex-none h-10 w-full bg-green-400 rounded mb-2 flex items-center justify-center '>
         <p>{phoneNumber}</p>
       </div>
-      <div className='flex-none h-20  rounded grid gap-3 grid-cols-4'>
+      <div className='flex-none h-20 w-full  rounded grid gap-3 grid-cols-4'>
         <div className='min-h-[50px] rounded bg-orange-500  flex items-center justify-center'>
           <p className='text-xs sm:text-base lg:text-2xl p-1 text-center'>
             Amount to pay
